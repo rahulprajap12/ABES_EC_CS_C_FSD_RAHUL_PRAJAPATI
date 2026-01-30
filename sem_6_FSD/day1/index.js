7b447e9c8e62bd5a8e09fba5c48834d5654b4ac7
rahulprajap12 <rahulprajapati1234123@gmail.com>
2026-01-30 09:37:17 +0530

    this is my promis cose

--- sem_6_FSD/day1/index.js
+++ sem_6_FSD/day1/index.js
@@ -6,4 +6,24 @@ const myPromise = new Promise((resolve, reject) => {
         reject("You failed!");
     }
 })
-myPromise.then((msg) => console.log(msg)).catch((error) => console.log("some error occurred",error));
+// myPromise.then((msg) => console.log(msg)).catch((error) => console.log("some error occurred",error));
+
+
+const f1 = async()=>{
+    const msg=await myPromise;
+    console.log(msg);
+}
+f1();
+
+
+
+const f2 = async()=>{
+    try{
+    const msg=await myPromise;
+    console.log(msg);
+    }catch(error){
+        console.log(error);
+    }
+    
+}
+f2();
