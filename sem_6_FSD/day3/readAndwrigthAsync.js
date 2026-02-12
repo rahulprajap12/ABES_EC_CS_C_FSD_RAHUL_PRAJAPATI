710ae7a6bc21fc17030cd3d53ec7e46090dfe06b
rahulprajap12 <rahulprajapati1234123@gmail.com>
2026-02-12 12:52:20 +0530

    make new dir

--- sem_6_FSD/day3/readAndwrigthAsync.js
+++ sem_6_FSD/day3/readAndwrigthAsync.js
@@ -1,14 +1,52 @@
-import fs from 'fs/promises';
+// import fs from 'fs/promises';
 
-async function fileWork() {
+// async function fileWork() {
+//     try {
+//         const data = await fs.readFile('data.txt', 'utf8');
+//         console.log(data);
+
+//         await fs.writeFile('data.txt', 'Updated Data');
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+// fileWork();
+
+import fs from "fs/promises"
+const appendFile = async (path, data) => {
     try {
-        const data = await fs.readFile('data.txt', 'utf8');
-        console.log(data);
+        await fs.appendFile(path, data);
+        console.log("Data has been appended successfully.");
+    } catch (error) {
+        console.log("unable to append file");
+    }
+}
+appendFile("./example.txt","this data has been appended")
 
-        await fs.writeFile('data.txt', 'Updated Data');
-    } catch (err) {
-        console.log(err);
+
+
+const readFile = async (path) => {
+    try {
+        const data = await fs.readFile(path, "utf-8");
+        console.log(data);
+    } catch (error) {
+        console.log("unable to read");
     }
 }
 
-fileWork();
+
+
+
+
+const writeFile = async (path, data) => {
+    try {
+        await fs.writeFile(path, data);
+        console.log("Data has been written successfully.");
+    } catch (error) {
+        console.log("Unable to write");
+    }
+}
+
+
+
